112c83680d4818bf061bcf93e73600344e086155
Devansh Jain <39978963+devanshjain14@users.noreply.github.com>
2020-12-09 00:58:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
